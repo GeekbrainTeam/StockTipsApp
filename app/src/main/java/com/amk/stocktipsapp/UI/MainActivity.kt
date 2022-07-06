dc2fc7ef442bd1c0f34ef8292420389b209cb1f6
hayarovy <edurd@rambler.ru>
2022-07-06 20:15:32 +0500

    make theme

--- app/src/main/java/com/amk/stocktipsapp/UI/MainActivity.kt
+++ app/src/main/java/com/amk/stocktipsapp/UI/MainActivity.kt
@@ -3,6 +3,7 @@ package com.amk.stocktipsapp.UI
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
