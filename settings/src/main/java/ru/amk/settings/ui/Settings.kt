f2aee4d8a5f0bda899edafdf43517d67d39e93b2
Mikhail <mkvgeekbrains@mail.ru>
2022-07-06 22:14:20 +0300

    Add module settings.

--- settings/src/main/java/ru/amk/settings/ui/Settings.kt
+++ settings/src/main/java/ru/amk/settings/ui/Settings.kt
@@ -1,11 +1,11 @@
-package com.amk.stocktipsapp.ui
+package ru.amk.settings.ui
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.amk.stocktipsapp.databinding.FragmentSettingsBinding
+import ru.amk.settings.databinding.FragmentSettingsBinding
 
 class Settings : Fragment() {
     private var _binding: FragmentSettingsBinding? = null
