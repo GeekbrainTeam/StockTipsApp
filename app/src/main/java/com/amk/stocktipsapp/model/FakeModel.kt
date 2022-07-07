65828e13557c155fbf3629874585cd66a80f5bab
GeekbrainTeam <107771110+GeekbrainTeam@users.noreply.github.com>
2022-07-07 07:01:13 +0300

    Revert "Gb 37 theme style"

--- app/src/main/java/com/amk/stocktipsapp/model/FakeModel.kt
+++
@@ -1,9 +0,0 @@
-package com.amk.stocktipsapp.model
-
-import java.io.Serializable
-
-data class FakeModel(
-    val fakeName: String,
-    val fakeTotal: Double,
-    val fakeFavorite: Boolean
-): Serializable
