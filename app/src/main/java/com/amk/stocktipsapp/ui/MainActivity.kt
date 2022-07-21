e57bec196e9c7b3a5f18b35fd8fe227aaeb947dd
hayarovy <edurd@rambler.ru>
2022-07-21 07:27:06 +0500

    switch dont correctly worked. must be continue

--- app/src/main/java/com/amk/stocktipsapp/ui/MainActivity.kt
+++ app/src/main/java/com/amk/stocktipsapp/ui/MainActivity.kt
@@ -1,31 +1,37 @@
 package com.amk.stocktipsapp.ui
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.amk.core.navigation.AppNavigation
+import com.amk.mylibrary.ui.ListCompanyFragment
+import com.amk.mylibrary.utils.KEY_PREF_THEME
 import com.amk.stocktipsapp.R
 import com.amk.stocktipsapp.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import org.koin.android.ext.android.inject
 import org.koin.core.parameter.parametersOf
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var navView: BottomNavigationView
     private val coordinator: AppNavigation by inject { parametersOf(this) }
+    private val sharedPrefs by lazy {  getSharedPreferences(KEY_PREF_THEME, Context.MODE_PRIVATE) }
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        initThemeListener()
         navView = binding.bottomNavigationView
-
         val appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.go_to_home,
@@ -45,11 +51,30 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
+
+    private fun initThemeListener() {
+        when (sharedPrefs.getInt(KEY_PREF_THEME, 0)) {
+            1 -> setCurrentTheme(AppCompatDelegate.MODE_NIGHT_NO)
+            2 -> setCurrentTheme(AppCompatDelegate.MODE_NIGHT_YES)
+            3 -> setCurrentTheme(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+        }
+    }
+
+    private fun setCurrentTheme(themeMode: Int) {
+        AppCompatDelegate.setDefaultNightMode(themeMode)
+    }
+
     private fun showBottomNav() {
         navView.visibility = View.VISIBLE
     }
     private fun hideBottomNav() {
         navView.visibility = View.GONE
+    }
+
+    override fun onPostResume() {
+        coordinator.navController.navigate(R.id.nav_graph)
+        super.onPostResume()
+
     }
 }
 
